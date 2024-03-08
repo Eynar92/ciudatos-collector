ecadb9602b4ff4b05cff2f2145c5531210a6fd84
Joel Alvarez <eynar.j.alvarez.m@gmail.com>
2024-03-08 17:23:06 -0400

    fix/Fixed modal width.

--- src/components/dashboard/form/CreateFormBtn.tsx
+++ src/components/dashboard/form/CreateFormBtn.tsx
@@ -6,7 +6,7 @@ export const CreateFormBtn = () => {
     return (
         <Button
             variant={"outline"}
-            className="h-48 flex flex-col items-center justify-center gap-4 group border border-primary/20 hover:cursor-pointer border-dashed"
+            className="h-48 w-full flex flex-col items-center justify-center gap-4 group border border-primary/20 hover:cursor-pointer border-dashed"
         >
             <FilePlusIcon className="h-8 w-8 text-muted-foreground group-hover:text-primary" />
             <span className="font-bold text-xl text-muted-foreground group-hover:text-primary">Crear un nuevo formulario</span>
